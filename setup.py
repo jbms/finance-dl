cd19e0849759cf9c09ecbb964cec4bb60bc5a8f3
Jeremy Maitin-Shepard <jeremy@jeremyms.com>
2019-03-03 16:09:13 -0800

    chore: update package version to 1.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='Tools for scraping personal financial data.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.1.0',
+    version='1.2.0',
     url='https://github.com/jbms/finance-dl',
     author='Jeremy Maitin-Shepard',
     author_email="jeremy@jeremyms.com",
