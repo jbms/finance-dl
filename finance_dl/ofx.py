jbms/finance-dl

commit cbf53ab3041043fe41fb1a019defc71497f11d5f

fix(ofx): handle line-terminated OFX tags in get_ofx_date_range
Thanks to Filippo Valsorda (@FiloSottile) for providing an initial fix.

finance_dl/ofx.py
@@ -119,7 +119,7 @@ import dateutil.parser
 import ofxclient.institution
 import ofxclient
 
-from beancount.ingest.importers.ofx import parse_ofx_time
+from beancount.ingest.importers.ofx import parse_ofx_time, find_child
 
 warnings.filterwarnings('ignore', message='split()', module='re')
 
@@ -143,19 +143,13 @@ def download_account_data_starting_from(account: ofxclient.account.Account,
 
 def get_ofx_date_range(data: bytes):
     soup = bs4.BeautifulSoup(io.BytesIO(data), 'html.parser')
-    dtstart_nodes = list(soup.find_all('dtstart'))
-    dtend_nodes = list(soup.find_all('dtend'))
-    if len(dtstart_nodes) == 0 or len(dtend_nodes) == 0:
+    dtstart = find_child(soup, 'dtstart', parse_ofx_time)
+    dtend = find_child(soup, 'dtstart', parse_ofx_time)
+    if dtstart is None or dtend is None:
         logger.debug('Data received: %r', data)
         messages = soup.find_all('message')
         logger.info('Messages: %r', [message.text for message in messages])
         return None
-    if len(dtstart_nodes) != 1 or len(dtend_nodes) != 1:
-        raise RuntimeError(
-            'More than one dtstart or dtend found in OFX document: %s' %
-            (data, ))
-    dtstart = parse_ofx_time(dtstart_nodes[0].text)
-    dtend = parse_ofx_time(dtend_nodes[0].text)
     return dtstart, dtend
 
 

commit d516bc50c33d4a98412e02957977533282a2b37d

fix(ofx): fix bug in ofx file naming
Commit cbf53ab3041043fe41fb1a019defc71497f11d5f introduced a bug
whereby downloaded OFX files were saved with an incorrect path,
leading to unnecessary additional data being downloaded.

finance_dl/ofx.py
@@ -145,7 +145,7 @@ def download_account_data_starting_from(account: ofxclient.account.Account,
 def get_ofx_date_range(data: bytes):
     soup = bs4.BeautifulSoup(io.BytesIO(data), 'html.parser')
     dtstart = find_child(soup, 'dtstart', parse_ofx_time)
-    dtend = find_child(soup, 'dtstart', parse_ofx_time)
+    dtend = find_child(soup, 'dtend', parse_ofx_time)
     if dtstart is None or dtend is None:
         logger.debug('Data received: %r', data)
         messages = soup.find_all('message')

finance_dl/ofx_rename.py
@@ -1,0 +1,37 @@
+"""Renames improperly-named OFX files generated by the finance_dl.ofx module"""
+import argparse
+import os
+
+import bs4
+
+from .ofx import get_ofx_date_range
+
+
+def fix_name(path, dry_run):
+    name = os.path.basename(path)
+    d = os.path.dirname(path)
+    date_format = '%Y%m%d'
+
+    parts = name.split('-')
+    assert len(parts) == 4
+
+    with open(path, 'rb') as f:
+        date_range = get_ofx_date_range(f.read())
+    new_parts = [
+        date_range[0].strftime(date_format), date_range[1].strftime(date_format)
+    ] + parts[2:]
+    new_name = '-'.join(new_parts)
+    if new_name != name:
+        new_path = os.path.join(d, new_name)
+        print('Rename %s -> %s' % (path, new_path))
+        if not dry_run:
+            os.rename(path, new_path)
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('paths', nargs='*')
+    args = ap.parse_args()
+    ap.add_argument('--dry-run', action='store_true')
+    for path in args.paths:
+        fix_name(path, dry_run=args.dry_run)