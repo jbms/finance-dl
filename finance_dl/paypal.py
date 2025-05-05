3c4d1be08cde9f9ca9729058b1a3782a152676af
moritzj29 <18733473+moritzj29@users.noreply.github.com>
2025-05-05 20:31:50 +0200

    [paypal] fix CSRF token retrieval (#106)

--- finance_dl/paypal.py
+++ finance_dl/paypal.py
@@ -188,10 +188,11 @@ class Scraper(scrape_lib.Scraper):
         logging.info('Getting CSRF token')
         self.driver.get('https://www.paypal.com/myaccount/transactions/')
         # Get CSRF token
-        body_element, = self.wait_and_locate((By.ID, "__APP_DATA__"))
-        data = base64.b64decode(body_element.get_attribute("innerHTML"))
+        body_element, = self.wait_and_locate((By.ID, "__react_data__"))
+        data = base64.b64decode(body_element.get_attribute("data"))
         attribute_object = json.loads(data)
         self.csrf_token = attribute_object["_csrf"]
+        logging.info('CSRF token retrieved')
         return self.csrf_token
 
     def get_transaction_list(self):
